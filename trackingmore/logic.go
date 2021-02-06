d85a103a320749751c8224df40338aa605250437
Mark Kop <marcelokopmann@gmail.com>
2021-02-05 21:00:31 -0300

    feat: make a request inside a service

--- trackingmore/logic.go
+++ trackingmore/logic.go
@@ -2,6 +2,8 @@ package trackingmore
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/go-kit/kit/log"
 )
@@ -20,5 +22,16 @@ func NewService(logger log.Logger) Service {
 func (s service) GetTracking(ctx context.Context, id string) (string, error) {
 	logger := log.With(s.logger, "method", "GetUser")
 	logger.Log("got tracking")
-	return id, nil
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		logger.Log(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Log(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	return sb, nil
 }
